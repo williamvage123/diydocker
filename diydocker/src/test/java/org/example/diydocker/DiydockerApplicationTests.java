eda92f70f2cad2ae40bd7f999d216fa1a09d5036
william vage <william.vage@gmail.com>
2024-10-01 08:54:13 +0200

    added project

---
+++ diydocker/src/test/java/org/example/diydocker/DiydockerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.diydocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiydockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
